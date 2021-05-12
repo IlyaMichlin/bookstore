0997b2c1bb069c6453f9bed07ed7d8423bcac951
Ilya <ilyamichlin1@gmail.com>
2021-05-12 23:55:14 +0300

    Added Bootstrap and Crispy

--- pages/tests.py
+++ pages/tests.py
@@ -1,10 +1,10 @@
 from django.test import SimpleTestCase  # for webpages with no model
 from django.urls import reverse, resolve
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 # Create your tests here.
-class HomepageTest(SimpleTestCase):
+class HomePageTest(SimpleTestCase):
     def setUp(self):
         url = reverse('home')
         self.response = self.client.get(url)
@@ -26,4 +26,29 @@ class HomepageTest(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contains_incorrect_html(self):
+        self.assertNotContains(self.response, 'Text not in homepage.')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
         )
