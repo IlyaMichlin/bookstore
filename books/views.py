ebcf87e96cb89ea6e299f8ede06fc4cd28dd6273
Ilya <ilyamichlin1@gmail.com>
2021-06-20 18:29:11 +0300

    Created static/images folder

--- books/views.py
+++ books/views.py
@@ -24,7 +24,6 @@ class SearchResultList(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/search_results.html'
-    # queryset = Book.objects.filter(title__icontains='First')
 
     def get_queryset(self):
         query = self.request.GET.get('q')
