e7194a1330e131e1f5b169d7d47c28374aa4fe6c
Ilya <damen63@gmail.com>
2021-06-26 20:34:44 +0300

    Bug test

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from environs import Env
 import socket
@@ -150,7 +150,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media
 MEDIA_URL = '/media/'  # absolute file system path
-MEDIA_ROOT = str(BASE_DIR.joinpath('media'))  # URL for templates
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
